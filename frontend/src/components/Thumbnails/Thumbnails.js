2926fcedeffcbc390e844779c820ffef9cafb6ca
Ivan Turk <iturk1@hrsume.hr>
2024-08-20 11:12:41 +0200

    css_editirano

--- frontend/src/components/Thumbnails/Thumbnails.js
+++ frontend/src/components/Thumbnails/Thumbnails.js
@@ -14,29 +14,30 @@ export default function Thumbnails({ foods }) {
               src={`/foods/${food.imageUrl}`}
               alt={food.name}
             />
-          </Link>
-          <div className={classes.content}>
-            <div className={classes.name}>{food.name}</div>
-            <span
-              className={`${classes.favorite} ${
-                food.favorite ? "" : classes.not
-              }`}
-            >
-              ❤
-            </span>
-            <div className={classes.stars}>
-              <StarRating stars={food.stars} />
-            </div>
-            <div className={classes.product_item_footer}>
-              <div className={classes.cook_time}>
-                <span>⏱️</span>
-                {food.cookTime}
+
+            <div className={classes.content}>
+              <div className={classes.name}>{food.name}</div>
+              <span
+                className={`${classes.favorite} ${
+                  food.favorite ? "" : classes.not
+                }`}
+              >
+                ❤
+              </span>
+              <div className={classes.stars}>
+                <StarRating stars={food.stars} />
+              </div>
+              <div className={classes.product_item_footer}>
+                <div className={classes.cook_time}>
+                  <span>⏱️</span>
+                  {food.cookTime}
+                </div>
+              </div>
+              <div className={classes.price}>
+                <Price price={food.price} />
               </div>
             </div>
-            <div className={classes.price}>
-              <Price price={food.price} />
-            </div>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>

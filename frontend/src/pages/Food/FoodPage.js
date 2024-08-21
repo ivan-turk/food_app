ea72e738e2754a9a70a0698d4699c7e101dfe1f7
Ivan Turk <iturk1@hrsume.hr>
2024-08-21 10:28:55 +0200

    implementacija_food_pagea_i_css_a

---
+++ frontend/src/pages/Food/FoodPage.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import classes from "./foodPage.module.css";
+import { useParams } from "react-router-dom";
+import { getById } from "../../services/foodService";
+import StarRating from "../../components/StarRating/StarRating";
+import Tags from "../../components/Tags/Tags";
+import Price from "../../components/Price/Price";
+
+export default function FoodPage() {
+  const [food, setFood] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    getById(id).then(setFood);
+  }, [id]);
+  return (
+    <>
+      {food && (
+        <div className={classes.container}>
+          <img
+            className={classes.image}
+            src={`/foods/${food.imageUrl}`}
+            alt={food.name}
+          />
+
+          <div className={classes.details}>
+            <div className={classes.header}>
+              <span className={classes.name}>{food.name}</span>
+              <span
+                className={`${classes.favorite} ${
+                  food.favorite ? "" : classes.not
+                }`}
+              >
+                ❤️
+              </span>
+            </div>
+
+            {/*Prikaz ocjene hrane*/}
+            <div className={classes.rating}>
+              <StarRating stars={food.stars} size={25} />
+            </div>
+
+            {/*Prikaz sastojaka pojedine hrane iz data.js*/}
+            <div className={classes.ingredients}>
+              {food.ingredients?.map((ingredient) => (
+                <span key={ingredient}>{ingredient}</span>
+              ))}
+            </div>
+
+            {/*Obrazložiti -> tagovi su objekti i stringovi...*/}
+            <div className={classes.tags}>
+              {food.tags && (
+                <Tags
+                  tags={food.tags.map((tag) => ({ name: tag }))}
+                  forFoodPage={true}
+                />
+              )}
+            </div>
+
+            {/*Prikaz vremena pripreme hrane iz data.js*/}
+            <div className={classes.cook_time}>
+              <span>
+                Vrijeme pripreme oko <strong>{food.cookTime}</strong>
+              </span>
+            </div>
+
+            {/*Prikaz cijene iz komponente*/}
+            <div className={classes.price}>
+              <Price price={food.price} />
+            </div>
+
+            <button>Dodaj u košaricu</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}

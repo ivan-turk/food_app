a3a81d209d1ea275a09cafb2e2fbef1309938957
Ivan Turk <iturk1@hrsume.hr>
2024-08-25 17:37:23 +0200

    login_api

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { sample_users } from "../data.js";
+import jwt from "jsonwebtoken";
+import { BAD_REQUEST } from "../constants/httpStatus.js";
 
 const router = Router();
 
@@ -8,4 +10,35 @@ router.post("/login", (req, res) => {
   const user = sample_users.find(
     (user) => user.email === email && user.password === password
   );
+
+  if (user) {
+    res.send(generateTokenResponse(user));
+    return;
+  }
+
+  res.status(BAD_REQUEST).send("Netočno korisničko ime ili lozinka!");
 });
+
+// generirani token za enkripciju podataka iz json web token modula
+const generateTokenResponse = (user) => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "Neki random text",
+    {
+      expiresIn: "30d",
+    }
+  );
+
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    adress: user.adress,
+    isAdmin: user.isAdmin,
+    token,
+  };
+};

9992d932b2feb271a5123042bd63976ab4e23ab5
Ivan Turk <iturk1@hrsume.hr>
2024-08-27 13:52:22 +0200

    order_page_edited

--- frontend/src/pages/Checkout/CheckoutPage.js
+++ frontend/src/pages/Checkout/CheckoutPage.js
@@ -1,5 +1,59 @@
 import React from "react";
+import { useCart } from "../../hooks/useCart";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { createOrder } from "../../services/orderService";
+import classes from "./checkoutPage.module.css";
+import Title from "../../components/Title/Title";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 export default function CheckoutPage() {
-  return <div>CheckoutPage</div>;
+  const { cart } = useCart();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const [order, setOrder] = useState({ ...cart });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const submit = async (data) => {
+    if (!order.addressLatLng) {
+      toast.warning("Molimo odaberite vašu lokaciju na karti!");
+      return;
+    }
+
+    await createOrder({ ...order, name: data.name, address: data.address });
+    navigate("/payment");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(submit)} className={classes.container}>
+        <div className={classes.content}>
+          <Title title="Narudžba odabranih artikala" fontSize="1.6rem" />
+          <div className={classes.inputs}>
+            <Input
+              defaultValue={user.name}
+              label="Ime"
+              {...register("name")}
+              error={errors.name}
+            />
+            <Input
+              defaultValue={user.address}
+              label="Adresa"
+              {...register("address")}
+              error={errors.address}
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }

cf3509883aa82c339fa8eb1f91faa279f1d11607
Ivan Turk <iturk1@hrsume.hr>
2024-08-27 14:37:17 +0200

    dodani_artikli_u_narudzbu

--- frontend/src/pages/Checkout/CheckoutPage.js
+++ frontend/src/pages/Checkout/CheckoutPage.js
@@ -10,6 +10,7 @@ import classes from "./checkoutPage.module.css";
 import Title from "../../components/Title/Title";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import OrderItemsList from "../../components/OrderItemsList/OrderItemsList";
 
 export default function CheckoutPage() {
   const { cart } = useCart();
@@ -50,6 +51,22 @@ export default function CheckoutPage() {
               label="Adresa"
               {...register("address")}
               error={errors.address}
+            />
+          </div>
+          <OrderItemsList order={order} />
+        </div>
+
+        <div>
+          <Title title="Izaberite vašu lokaciju za dostavu" fontSize="1.6rem" />
+        </div>
+
+        <div className={classes.buttons_container}>
+          <div className={classes.buttons}>
+            <Button
+              type="submit"
+              text="Idi na naplatu"
+              width="100%"
+              height="3rem"
             />
           </div>
         </div>

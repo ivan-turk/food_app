83ee5b767b19d7948864dce12aa5e670f271f140
Ivan Turk <iturk1@hrsume.hr>
2024-09-02 14:34:27 +0200

    admin_dashboard_finished

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://10.10.0.75:3000"],
   })
 );
 
